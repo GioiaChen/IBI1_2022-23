532e10eba87990971f23001592d4d1359f78cbd6
GioiaChen <127113630+GioiaChen@users.noreply.github.com>
2023-03-14 15:43:13 +0800

    Create variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,20 @@
+a = -3.19
+b = -118.24
+c = 116.39
+d = a-b
+print(d)
+e = a-c
+print(e)
+# Is d greater than or equal to e?
+d >= e
+# Is d greater than e?
+d > e
+# d is greater than e. Rob	travel	further	to	Los	Angeles than to Haining.
+X = True
+Y = False
+W = X and Y
+print(W)
+# W = False
+Z = X or Y
+print(Z)
+# Z = True
