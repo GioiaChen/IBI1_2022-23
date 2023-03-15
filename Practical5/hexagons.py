bb8c121d2bbdd290bb51d0fe2da4f4f62134636b
Zhuying Chen <zhuying.22@intl.zju.edu.cn>
2023-03-15 15:24:18 +0800

    2

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,5 +1,6 @@
-# print the first 5 hexagonal	number.
+# print the first 5 hexagonal number with a for-loop
 for n in range(1,6):
+ # put into the formula
  h = 2*n*(2*n-1)/2
  print (h)
 print ("Down")
