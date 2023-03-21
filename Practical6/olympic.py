ab9745b8595aefc9152f9a22377ec1e8cf5b5051
Zhuying Chen <zhuying.22@intl.zju.edu.cn>
2023-03-21 16:19:54 +0800

    4

---
+++ Practical6/olympic.py
@@ -0,0 +1,5 @@
+# Create a dictionay that contain the cost of each Olympic games, print the cost of the input one.
+Cost = {'Los Angeles 1984':1,'Seoul 1988':8,'Barcelona 1992':15}
+print (Cost['Barcelona 1992'])
+costs=[1,8,15,7,5,14,43,40]
+
