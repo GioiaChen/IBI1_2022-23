a93ab4ce0deae291ff3d67a1081d82e0344f3758
GioiaChen <127113630+GioiaChen@users.noreply.github.com>
2023-03-21 16:47:54 +0800

    Update and rename 1.py to Pie_Chart.py

--- Practical6/Pie_Chart.py
+++ Practical6/Pie_Chart.py
@@ -1,11 +1,8 @@
-# Import the matplotlib and the numpy
+# Import the matplotlib and the numpy, use the data to draw a pie chart then specifies the fraction of the radius with which to offset each wedge.
 import matplotlib.pyplot as plt
-# Try to use the data to draw a pie chart
 labels = 'Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Documentary','History','War'
 sizes = [73,42,38,28,22,19,18,12,8,7]
 explode = (0.1,0,0,0,0,0,0,0,0,0)
-# Specifies the fraction of the radius with which to offset each wedge
 plt.pie (sizes, explode = explode,labels = labels,autopct = '%1.1f%%',shadow = False,startangle = 90)
 plt.axis('equal')
 plt.show()
-
