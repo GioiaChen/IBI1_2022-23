a279f065556d376585d3687fe2d6bd4f1b582ae0
Zhuying Chen <zhuying.22@intl.zju.edu.cn>
2023-03-21 15:25:33 +0800

    q

---
+++ Practical6/Movie_genre.py
@@ -0,0 +1,7 @@
+# Create a dictionary with the movie genre and their number of studentsfor which this genre is their favorite
+Movie_genre = {'Comedy':73,'Action':42,'Romance':38,'Fantasy':28,'Science-fiction':22,'Horror':19,'Crime':18,'Documentary':12,'History':8}
+# Add the last movie genre into the dictionary
+Movie_genre['War'] = 7
+# Print the dictionary of Movie genre
+print(Movie_genre)
+
