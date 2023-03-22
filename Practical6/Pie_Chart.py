735fe529c2ead2b68c73abccd6a2338acc866733
Zhuying Chen <zhuying.22@intl.zju.edu.cn>
2023-03-22 15:23:00 +0800

    1

--- Practical6/Pie_Chart.py
+++ Practical6/Pie_Chart.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 labels = 'Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Documentary','History','War'
 sizes = [73,42,38,28,22,19,18,12,8,7]
-explode = (0.1,0,0,0,0,0,0,0,0,0)
-plt.pie (sizes, explode = explode,labels = labels,autopct = '%1.1f%%',shadow = False,startangle = 90)
+explode = (0,0,0,0,0,0,0,0,0,0)
+plt.pie(sizes, explode = explode,labels = labels,autopct = '%1.1f%%',shadow = False,startangle = 90)
 plt.axis('equal')
 plt.show()
