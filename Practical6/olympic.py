9d132838c0308f96c8919d855dbb385a7eaabac3
GioiaChen <127113630+GioiaChen@users.noreply.github.com>
2023-03-22 16:31:03 +0800

    Update olympic.py

--- Practical6/olympic.py
+++ Practical6/olympic.py
@@ -1,5 +1,4 @@
-# Create a dictionay that contain the cost of each Olympic games, print the cost of the input one.
-Cost = {'Los Angeles 1984':1,'Seoul 1988':8,'Barcelona 1992':15}
-print (Cost['Barcelona 1992'])
-costs=[1,8,15,7,5,14,43,40]
-
+# Create a list that contain the cost of each Olympic games, print the cost of the input one.
+costs = [1,8,15,7,5,14,43,40]
+costs = sorted(costs)
+print(costs)
