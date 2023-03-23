5fbadd85b851b9f66bd3f8ad684cd0a7ce149196
GioiaChen <127113630+GioiaChen@users.noreply.github.com>
2023-03-23 21:44:31 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,14 +1,15 @@
-# import a variable to count the generation
+# Import a variable to count the generation
 i = 1
 print ("Generation ",i)
-# import a variable that carries the number of rabbits
+# Import a variable that carries the number of rabbits
 j = 2
 print (j)
+
 # Use a while-loop to achieve the output of every generation
 while 1==1:
  i += 1
  print ("Generation ",i)
- # the number of rabbits in every generation is the two times of the previous one
+ # The number of rabbits in every generation is the two times of the previous one.
  j = j*2
  print (j)
  if j*2 > 100:
