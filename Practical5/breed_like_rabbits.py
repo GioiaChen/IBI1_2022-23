9c2a0ec70f6cfbcaa66426c5e472256efa4dcba0
GioiaChen <127113630+GioiaChen@users.noreply.github.com>
2023-03-23 21:52:24 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -5,7 +5,7 @@ print ("Generation ",i)
 j = 2
 print (j)
 
-# Use a while-loop to achieve the output of every generation
+# Use a while-loop to achieve the output of every generation with less than 100 rabbits: print each generation followed by the number of rabbits of that generation
 while 1==1:
  i += 1
  print ("Generation ",i)
@@ -13,5 +13,5 @@ while 1==1:
  j = j*2
  print (j)
  if j*2 > 100:
-  break
-print ("At the",i+1,"th generation over 100 rabbits have been born.")
+  break  # break the while-loop when the next generation will be more than 100 
+print ("At the",i+1,"th generation over 100 rabbits have been born.")  # The generation with more than 100 rabbits born will be the (i+1)th one.
