1c26c603e9718c932b61c2f1970503cf6b8f36b0
GioiaChen <127113630+GioiaChen@users.noreply.github.com>
2023-03-23 18:37:38 +0800

    Update Movie_genre.py

--- Practical6/Movie_genre.py
+++ Practical6/Movie_genre.py
@@ -7,3 +7,14 @@ print(Movie_genre)
 # Input a kink of movie genre and output the corresponding number.
 input = 'Comedy'
 print(Movie_genre[input])
+
+# Import the matplotlib and use the data to draw a pie chart.
+import matplotlib.pyplot as plt
+labels = 'Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Documentary','History','War'
+sizes = [73,42,38,28,22,19,18,12,8,7]
+explode = (0,0,0,0,0,0,0,0,0,0)
+colors = ['azure','lightcyan','paleturquoise','c','darkcyan','darkslategray','darkgreen','g','forestgreen','lightgreen']
+# Specifies the fraction of the radius with which to offset each wedge.
+plt.pie(sizes, explode = explode,labels = labels,autopct = '%1.1f%%',shadow = False,startangle = 90,colors = colors)
+plt.axis('equal')
+plt.show()
