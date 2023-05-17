b13b20a65650a198c080064d07f2b4cc444199be
Zhuying Chen <zhuying.22@intl.zju.edu.cn>
2023-05-17 20:35:03 +0800

    2nd version

--- Practical10/Mortgage_affordability_calculator.py
+++ Practical10/Mortgage_affordability_calculator.py
@@ -1,10 +1,11 @@
+# Define a function to compare the price and salary.
 def calculate(x, y):
     if x > y*5:
         print("NO! Save more money for buying a house please.")
     else:
         print("Yes")
 
-
+# Input the price and salary for comparison.
 price = input("Price = ")
 salary = input("Salary = ")
 calculate(price, salary)
